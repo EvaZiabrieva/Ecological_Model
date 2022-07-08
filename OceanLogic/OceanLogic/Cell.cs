d46bfec3dea5b5c4360edc090ee3e2ce572a0519
Eva Ziabrieva <evazyabrevaaa@gmail.com>
2022-07-08 14:26:52 +0300

    add dll with ocean logic

---
+++ OceanLogic/OceanLogic/Cell.cs
@@ -0,0 +1,15 @@
+﻿namespace OceanLogic
+{
+    public abstract class Cell
+    {
+        protected readonly Ocean ocean;
+
+        public Cell(Ocean ocean)
+        {
+            this.ocean = ocean;
+        }
+
+        public abstract char GetSymbol();
+        public abstract void Iterate(int i, int j);
+    }
+}
